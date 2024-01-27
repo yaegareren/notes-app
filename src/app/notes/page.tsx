3c9c28810a080f170bab2fc4c3307d02960dfd4c
Shree Mangalam <sshreemangalam@gmail.com>
2024-01-28 01:20:45 +0530

    setup authenticatioh

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "NotesApp - Notes",
+};
+
+const Notespage = () => {
+  return <div>here will be your notes</div>;
+};
+
+export default Notespage;
