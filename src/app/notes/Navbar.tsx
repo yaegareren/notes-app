fbc25399603afdaa90856eb594aa9170309c8a7d
Shree Mangalam <sshreemangalam@gmail.com>
2024-01-28 12:05:39 +0530

    built navbar and home pages

---
+++ src/app/notes/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logoipsum-280.svg";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <div className="p-4 shadow">
+      <div className="max-w-7xl m-auto flex flex-wrap gap-3 items-center justify-between">
+        <Link href={"/notes"} className="flex items-center gap-1">
+          <Image src={logo} alt="notesLogo" width={40} height={40} />
+          <span className="font-semibold">NotesApp</span>
+        </Link>
+        <div className="flex items-center gap-2">
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+            }}
+          />
+          <Button variant="secondary">
+            <Plus size={20} className="mr-2" /> Add Note
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import logo from "@/assets/logoipsum-280.svg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { userId } = auth();
+
+  if (userId) redirect("/notes");
+
   return (
     <>
-      <div className="text-blue-600">NOTES App</div>
+      <main className="flex flex-col h-screen items-center justify-center gap-5">
+        <div className="flex items-center gap-4">
+          <Image src={logo} alt="logo" width={100} height={100} />
+          <span className="font-bold tracking-tight text-4xl lg:text-5xl text-gray-700">
+            Notes Application
+          </span>
+        </div>
+        <p className="text-center max-w-prose">
+          a modern notes application built using next.js, mongodb, tailwindcss
+          and clerk
+        </p>
+        <Button size="lg" asChild>
+          <Link href="/notes">Open</Link>
+        </Button>
+      </main>
     </>
   );
 }
